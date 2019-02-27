4f6fdac2ea8213fa5c33a7605f178b503cf9aa4b
shadiaali <shadiamalfoy@gmail.com>
2019-02-26 23:57:25 -0500

    main pg done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 $(document).foundation();
+
+var $navList = $('.menu-fancy');
+
+$navList.on('click', 'li:not(.is-selected)', function(e){
+  $navList.find(".is-selected").removeClass("is-selected");
+  $(e.currentTarget).addClass("is-selected");
+});
